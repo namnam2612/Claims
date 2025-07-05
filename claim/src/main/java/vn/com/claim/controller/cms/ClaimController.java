562d57b4dafcfe8c17600263c52bb85b4bfbb31b
Java <danghoangnam2612@gmail.com>
2025-07-06 02:23:46 +0700

    Claims hi

--- claim/src/main/java/vn/com/claim/controller/cms/ClaimController.java
+++ claim/src/main/java/vn/com/claim/controller/cms/ClaimController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ClaimController {
 
+    //abcdef
 
     @GetMapping("/cms/claim-manager")
     public String claimManager() {
